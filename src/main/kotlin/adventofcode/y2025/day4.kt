5aff046062dafe3776f78d7afd310bc30f1e5975
Koen Konst <koen.konst@avisi.nl>
2025-12-04 15:26:15 +0100

    2025 day 4 optimized

--- src/main/kotlin/adventofcode/y2025/day4.kt
+++ src/main/kotlin/adventofcode/y2025/day4.kt
@@ -8,17 +8,15 @@ import adventofcode.utils.readInput
 fun main() {
     val lines = readInput("2025/day4/input.txt")
     val grid = lines.mapIndexed { rowIndex, row -> row.mapIndexed { columnIndex, ch -> PointWithChar(Point(rowIndex.toLong(), columnIndex.toLong()), ch) } }
-    val part1 = grid.flatten().count { point ->
-        point.value == '@' && grid.getNeighboursWithDiagonals(point.point).count { it.value == '@' } < 4
-    }
+    val part1 = grid.flatten().count { it.value == '@' && grid.getNeighboursWithDiagonals(it.point).count { it.value == '@' } < 4 }
 
     println("Part 1: $part1")
 
     var part2 = 0
     while (true) {
-        val result = grid.flatten().count { point ->
-            if (point.value == '@' && grid.getNeighboursWithDiagonals(point.point).count { it.value == '@' } < 4) {
-                point.value = '.'
+        val result = grid.flatten().count {
+            if (it.value == '@' && grid.getNeighboursWithDiagonals(it.point).count { it.value == '@' } < 4) {
+                it.value = '.'
                 true
             } else {
                 false
